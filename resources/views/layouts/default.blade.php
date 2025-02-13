68f58f2ad53ea41ee392fbcccac2a23c03090e9e
loftyyyy <lofty08.business@gmail.com>
2025-02-14 01:54:51 +0800

    hello

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>@yield('title', 'home')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Expanded:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
     <header class="p-1">
         <nav class="flex">
-            <h2 class="ml-36 rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">Second Gear</h2>
+            <h2 class="ml-36 font-encode text-2xl rounded-md px-3 py-2 text-white">Second Gear</h2>
             <li class="flex ml-auto mr-96">
                 <ul class="mx-6 rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"><a href="{{ route('login') }}">Log In</a></ul>
                 <ul class="mr-3 rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"><a href="{{ route("register") }}">Sign Up</a></ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                encode: ['"Encode Sans Expanded"', 'sans-serif'],
             },
         },
     },
