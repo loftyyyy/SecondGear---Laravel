d67d20afb8286aa8dabb00a370dc02073b0a65f4
Notanne1305 <j.sindo.545117@umindanao.edu.ph>
2025-02-26 17:36:44 +0800

    Fixed Background (Deprecated)

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -22,7 +22,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                 </svg>
 
-                <h2 class="text-[#F2F2F2] font-encode text-3xl rounded-md font-future">Second Gear</h2>
+                <h2 class="text-[#F2F2F2]  text-3xl rounded-md font-future">Second Gear</h2>
             </div>
 
 
@@ -42,7 +42,8 @@
 
     <main class="">
         <div id="intro" class="w-full relative pb-3 mx-auto bg-white-500 text-center z-10">
-            <img id="background" class="absolute inset-0 w-full h-auto z-0" src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background">
+            <svg xmlns="http://www.w3.org/2000/svg" class="absolute inset-0 w-full h-auto z-0" fill="none" viewBox="0 0 877 968"><g clip-path="url(#a)"><circle cx="391" cy="391" r="390.5" stroke="#FF2D20" transform="matrix(-1 0 0 1 416 -56)"/><circle cx="468" cy="468" r="467.5" stroke="#FF2D20" opacity=".3" transform="matrix(-1 0 0 1 493 -133)"/><circle cx="558" cy="558" r="557.5" stroke="#FF2D20" opacity=".1" transform="matrix(-1 0 0 1 583 -223)"/><g filter="url(#b)"> <ellipse cx="583" cy="229.5" fill="#FF2D20" rx="583" ry="229.5" transform="matrix(-1 0 0 1 621 -9)"/></g><g filter="url(#c)"><ellipse cx="262" cy="184.5" fill="#fff" rx="262" ry="184.5" transform="matrix(-1 0 0 1 99 42)"/></g></g><defs><filter id="b" width="1614" height="907" x="-769" y="-233" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur result="effect1_foregroundBlur_3089_39042" stdDeviation="112"/></filter><filter id="c" width="972" height="817" x="-649" y="-182" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur result="effect1_foregroundBlur_3089_39042" stdDeviation="112"/></filter><clipPath id="a"><path fill="#fff" d="M877 0H0v968h877z"/></clipPath></defs></svg>
+            <!-- <img id="background"  src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background"> -->
             <img id="porsche" src="picbank/landing page pic.png" alt="Red Porsche 911" class="relative mx-auto animate-[slideIn_0.5s_ease-out] z-10">
         </div>
 
