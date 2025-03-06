caa4ae8bd3c357566f3fa1fe1965e1e63cf3796d
loftyyyy <lofty08.business@gmail.com>
2025-03-06 17:01:19 +0800

    Charisse

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -24,7 +24,10 @@
         <div id="avatarButton" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start" class="z-50 relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
             <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
         </div>
+    
+        <!-- Sala ni charisse tanan -->
 
+        
         <div id="userDropdown" class="z-50 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
             <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
             <div>{{Auth::user()->name}}</div>
